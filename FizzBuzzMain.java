2e23d7e53c02e5a8c5dd4613f4dca473f96d5688
jar <thanatos.ja@gmail.com>
2024-05-10 21:03:51 -0600

    Ejercicio completo

---
+++ FizzBuzzMain.java
@@ -0,0 +1,14 @@
+package L4;
+
+public class FizzBuzzMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Creamos un objeto de la clase fizz buzz
+		FizzBuzz nuevobjetofb=new FizzBuzz();
+		//Llamamos el método imprimir para mostrar el resultado
+		nuevobjetofb.imprimir_hasta100();
+
+	}
+
+}
